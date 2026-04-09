034cfc5a5ed1fb72b82050e953ce1678ab8bb872
raian2209 <andradeneto1999@gmail.com>
2026-04-09 19:33:08 -0300

    Initial commit: Spring Initializr

---
+++ src/main/java/br/com/dunnastecnologia/chamados/GerenciadorChamadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.dunnastecnologia.chamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorChamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorChamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dunnastecnologia/chamados/GerenciadorChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dunnastecnologia.chamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
