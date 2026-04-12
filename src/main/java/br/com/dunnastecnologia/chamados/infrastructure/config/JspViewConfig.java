3b25cd86d52c6a9f9ac73111699f3d8e2852215e
raian2209 <andradeneto1999@gmail.com>
2026-04-11 22:19:14 -0300

    feat(Config): Para Definir configurações do JSPView

---
+++ src/main/java/br/com/dunnastecnologia/chamados/infrastructure/config/JspViewConfig.java
@@ -0,0 +1,21 @@
+package br.com.dunnastecnologia.chamados.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class JspViewConfig {
+
+    @Bean
+    public ViewResolver jspViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        resolver.setOrder(10);
+        return resolver;
+    }
+}
