c177d2dbe249c58aab595417f2d62c9a21871268
raian2209 <andradeneto1999@gmail.com>
2026-04-11 21:53:04 -0300

    feat(Security): adicionar jwtsevice e filter

--- src/main/java/br/com/dunnastecnologia/chamados/domain/model/AnexoChamado.java
+++ src/main/java/br/com/dunnastecnologia/chamados/domain/model/AnexoChamado.java
@@ -1,15 +1,10 @@
 package br.com.dunnastecnologia.chamados.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.util.UUID;
 
@@ -33,6 +28,7 @@ public class AnexoChamado {
 
     private Long tamanhoBytes;
 
-    @Lob
+    @JdbcTypeCode(SqlTypes.VARBINARY)
+    @Column(name = "conteudo")
     private byte[] conteudo;
 }
