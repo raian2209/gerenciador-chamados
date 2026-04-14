4414b3d03106a4c3074e73939a0e7aac04fcc61a
raian2209 <andradeneto1999@gmail.com>
2026-04-14 13:03:19 -0300

    feat(Spring): Variaveis de ambiente / enable scheduler

--- src/main/java/br/com/dunnastecnologia/chamados/GerenciadorChamadosApplication.java
+++ src/main/java/br/com/dunnastecnologia/chamados/GerenciadorChamadosApplication.java
@@ -2,8 +2,10 @@ package br.com.dunnastecnologia.chamados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GerenciadorChamadosApplication {
 
 	public static void main(String[] args) {
